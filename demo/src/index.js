2d29da27de305ba802cdd1ba6211906d8217aaf6
keyflight <olivier@andev.nl>
2019-04-28 15:10:49 +0700

    Initial commit

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,60 @@
 import React, {Component} from 'react'
 import {render} from 'react-dom'
+import {injectGlobal} from 'emotion'
+import {Canvas, Heading, Paragraph, Box} from '@cmds/demo-utils'
+import Button from '@cmds/button'
 
-import Example from '../../src'
+injectGlobal`
+    * {
+        box-sizing: border-box;
+    }
+    body {
+        font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol;
+        margin: 0;
+    }
+`
+
+import Spinner from '../../src'
 
 class Demo extends Component {
-  render() {
-    return <div>
-      <h1>spinner Demo</h1>
-      <Example/>
-    </div>
-  }
+    render() {
+        return (
+            <Canvas>
+                <Heading>
+                    Default
+                </Heading>
+                <Box>
+                    <Spinner/>
+                </Box>
+                <Heading>
+                    High speed
+                </Heading>
+                <Box>
+                    <Spinner speed={200}/>
+                </Box>
+                <Heading>
+                    Small
+                </Heading>
+                <Box>
+                    <Spinner width={20}/>
+                </Box>
+                <Heading>
+                    Large
+                </Heading>
+                <Box>
+                    <Spinner width={60}/>
+                </Box>
+                <Heading>
+                    Inside a button
+                </Heading>
+                <Box>
+                    <Button icon={Spinner}>
+                        With spinner
+                    </Button>
+                </Box>
+            </Canvas>
+        )
+    }
 }
 
 render(<Demo/>, document.querySelector('#demo'))
--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
-import React, {Component} from 'react'
+import React from 'react'
+import {css, cx, keyframes} from 'emotion'
 
-export default class extends Component {
-  render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
+const spin = keyframes`
+  0% {
+    transform: rotate(0deg);
   }
+  100% {
+    transform: rotate(359deg);
+  }   
+`
+
+const Spinner = ({className, size, width, height, speed}) => {
+
+    speed = speed || 600
+    size = size || width || height || 32
+
+    return (
+        <div
+            className={cx(
+                css`
+      display: inline-block;
+      width: ${size}px;
+      height: ${size}px;
+      border-radius: 50%;
+      border: 2px solid #D3DAE6;
+      border-color: #07f #D3DAE6 #D3DAE6 #D3DAE6;
+      animation: ${spin} ${speed}ms infinite linear;
+    `, className
+            )}
+        />
+    )
 }
+
+export default Spinner
